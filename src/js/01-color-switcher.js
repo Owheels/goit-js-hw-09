b36df125a9708381fad615bfa458348f014da9d8
Oleksandr Kalienchenko <owheels.qa@gmail.com>
2023-06-20 22:27:11 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBackground() {
+  const colorCode = getRandomHexColor();
+  document.body.style.background = colorCode;
+}
+
+function changeBackground() {
+  intervalId = setInterval(createBackground, 1000);
+  startBtn.disabled = true;
+}
+
+function stopChangeBackground() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', changeBackground);
+stopBtn.addEventListener('click', stopChangeBackground);
