dd7c094fb2c5064f0b35732628f96188cdd9ed2f
Owheels <owheels.qa@gmail.com>
2023-06-22 17:11:18 +0000

    Deploying to gh-pages from @ Owheels/goit-js-hw-09@47c2058ec689b8a0ed7aee7bbab6e34cf202ac9c 🚀

--- 01-color-switcher.1b6bd3da.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n;function a(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.background=t}t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.1b6bd3da.js.map
---
+++ 01-color-switcher.dcb41391.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;function a(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.background=t}e.disabled=!0,t.addEventListener("click",(function(){d=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.dcb41391.js.map
