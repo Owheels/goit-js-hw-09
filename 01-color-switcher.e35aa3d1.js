19daa3a357105ffd3986415fa8fd5290ab43d627
Owheels <owheels.qa@gmail.com>
2023-06-22 15:45:36 +0000

    Deploying to gh-pages from @ Owheels/goit-js-hw-09@42a679e4e9458e3216af95b5df90366a7c3d986c 🚀

---
+++ 01-color-switcher.e35aa3d1.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");function n(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.background=t}e.addEventListener("click",(function(){t=setInterval(n,1e3),e.disabled=!0})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.e35aa3d1.js.map
